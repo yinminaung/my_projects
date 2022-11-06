f549e44f4cb32f7500df4233e4c6b1b2203f13f1
yinminaung <yinminaung72@gmail.com>
2022-11-06 23:59:52 +0630

    add tick sound to analog clock

--- 51-analog-clock/script.js
+++ 51-analog-clock/script.js
@@ -2,6 +2,7 @@ setInterval(setClock, 1000)
 const hourHand = document.querySelector(".hour-hand");
 const minuteHand = document.querySelector(".minute-hand");
 const secondHand = document.querySelector(".second-hand");
+const tickSound = document.getElementById("tick");
 
 function setClock() {
     const currentDate = new Date();
@@ -16,6 +17,13 @@ function setClock() {
 
 function setRotation(element, rotate) {
     element.style.setProperty("--rotate", rotate * 360);
+    playSound();
+}
+
+function playSound() {
+    tickSound.currentTime = 0;
+    tickSound.volume = 0.1;
+    tickSound.play();
 }
 
 setClock();
